3ce5458471ce04e83f073281328fe282bcfa4545
muirkajika <moore.823@gmail.com>
2025-07-07 22:17:49 -0400

    Update next.config.js
    Feat: Enable standalone output for Docker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // This is the crucial line we are adding
+  output: 'standalone',
+}
 
 module.exports = nextConfig
